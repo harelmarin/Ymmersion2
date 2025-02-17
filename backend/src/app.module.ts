29819e85a121cce8547e9d538add4c9e76b6ccd9
harelmarin <145020856+harelmarin@users.noreply.github.com>
2025-02-17 10:34:09 +0100

    feat(prisma) : add prisma client
    + .env.example

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaClient } from '@prisma/client';
 
 @Module({
-  imports: [],
+  imports: [PrismaClient],
   controllers: [AppController],
   providers: [AppService],
 })
