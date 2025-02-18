223c5c4d0778258dc7fc5d30657474946c9f268f
yulan <yulan.nguyenvan@ynov.com>
2025-02-18 16:24:21 +0100

    dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { fetchStatisticsByMonth } from '../services/statisticService';
 
 const Dashboard = () => {
+  const { data, isLoading, error } = fetchStatisticsByMonth(new Date().getMonth() + 1, new Date().getFullYear())
+  console.log(data)
   return (
     <div>
       <h1>dashboard</h1>
